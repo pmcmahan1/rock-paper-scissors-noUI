175c060a2fd6a345683c7de3a48d2c34194d62e9
Patrick McMahan <mcmahanconner@gmail.com>
2022-07-28 21:15:37 +0900

    Finished simple single round game

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-// This function assigns the computer's choice of rock, paper, or scissorss
+// This function assigns the computer's choice of rock, paper, or scissors
 function getComputerChoice() {
     const number = Math.floor(Math.random() * 1000);
     if (number % 3 === 0) {
@@ -11,6 +11,41 @@ function getComputerChoice() {
     return 'scissors';
 }
 
+// This function runs a single game of rock, paper, or scissors
+function playGame(userSelection, computerSelection) {
+    if (computerSelection == userSelection) {
+        return 'Tie!';
+    }
+    else if (userSelection == 'rock' && computerSelection == 'scissors') {
+        return 'You win! rock beats scissors'
+    }
+    else if (userSelection == 'paper' && computerSelection == 'rock') {
+        return 'You win! paper beats rock'
+    }
+    else if (userSelection == 'scissors' && computerSelection == 'paper') {
+        return 'You win! scissors beats paper'
+    }
+    else {
+        return 'You lose! ' + computerSelection + " beats " + userSelection;
+    }
+}
+
+
+
 // Assigning the computer's choice to a variable we can use
 computerSelection = getComputerChoice();
-console.log(computerSelection);
+
+// Prompting the user to type either rock, paper, or scissors
+let userPrompt = prompt("Please input either rock, paper, or scissors");
+
+// Forces whatever user input into lower case
+let userSelection = userPrompt.toLowerCase();
+
+// Runs the game function from above
+playGame();
+
+// Shows the computers selection in the console, to make sure everything is working
+console.log(computerSelection)
+
+//Displays the game results in the console
+console.log(playGame(userSelection, computerSelection));
